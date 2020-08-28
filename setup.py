80150aa5b0f9f69b3e1ca2bc63aa4cc4e4279644
alexovai <teamvoxela@gmail.com>
2020-08-28 15:50:45 -0700

    adding requirements and modifying setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+packages = ['sensorflow', 'sensorflow.dataframe']
+
+tests = [p + '.tests' for p in packages]
+
+
+setup(name='sensorflow',
+      version='0.0.1',
+      description='Voxela Sensorflow API',
+      maintainer='alexov',
+      maintainer_email='alexov@voxela.ai',
+      keywords='voxela',
+      packages=packages + tests,
+      python_requires='>=3.6',
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      zip_safe=False)
